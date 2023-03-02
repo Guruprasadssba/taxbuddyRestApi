680256105cea4b33fea0c2d1d7bbd3ca56dfaf3a
gurup <guruprasad.n@ssbainnovations.com>
2023-03-02 18:24:49 +0530

    new changes

--- RestApiTaxbudy/src/main/java/generic_Utility/ExtentTestManager.java
+++ RestApiTaxbudy/src/main/java/generic_Utility/ExtentTestManager.java
@@ -9,7 +9,7 @@ import com.relevantcodes.extentreports.ExtentTest;
 public class ExtentTestManager 
 {
 
-	static Map extentTestMap = new HashMap();
+/*	static Map extentTestMap = new HashMap();
 	static ExtentReports extent = ExtentManager.getReporter();
 
 	public static synchronized ExtentTest getTest() {
@@ -29,5 +29,5 @@ public class ExtentTestManager
 		extentTestMap.put((int) (long) (Thread.currentThread().getId()), test);
 
 		return test;
-	}
+	}*/
 }
